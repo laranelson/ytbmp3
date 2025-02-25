781ddc9b2c0731854de028ad34e5d17ad0c0a078
Nelson Lara <nelsonlarajr@gmail.com>
2025-02-25 15:05:38 -0300

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import sys
+import os
 
-def create_app():
-    app = Flask(__name__)
+# Adiciona o diretório do projeto ao sys.path
+sys.path.insert(0, os.path.dirname(__file__))
 
-    return app
+# Importa a função create_app
+from app import create_app
+
+# Cria a aplicação
+application = create_app()  # Agora o Passenger usará a instância `application`
