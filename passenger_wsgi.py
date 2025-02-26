fb407b75322661721886fbcaae6a2f0eaacf8e2a
Nelson Lara <nelsonlarajr@gmail.com>
2025-02-26 14:14:28 -0300

    alterei app.py para server.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,11 @@
 import sys
 import os
 
-# Adiciona o diretório do projeto ao sys.path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Imprimir sys.path para depuração
-print(sys.path)
+# Importa a variável 'app' do server.py
+from server import app as application
 
-# Importa a função create_app de app/__init__.py
-from app import create_app
 
-# Cria a aplicação
-application = create_app()
+
 
